719cab75de68ba71952f8e592d1a91558d990568
William Owen <william.owen.bsc@gmail.com>
2021-10-23 10:56:34 -0700

    got math happening

--- src/objects/Calculator.js
+++ src/objects/Calculator.js
@@ -5,15 +5,15 @@ export default class Calculator {
   screen;
   currentView = {
     text: "",
-    number: null
+    number: ''
   };
   addition = {
     text: '',
-    number: null,
+    number: '',
     operator: ''
   };
-  intProxy = "";
-  remainProxy = "";
+  // intProxy = "";
+  // remainProxy = "";
 
   constructor(el) {
     this.screen = el.querySelector('.total');
@@ -21,7 +21,7 @@ export default class Calculator {
     el.addEventListener('click', this.handleClick);
     this.configure()
 
-    
+    // console.log(exactMath.formula('2 * -3 + 4 + 5'));
   }
 
   processToComma = (string) => {
@@ -54,134 +54,134 @@ export default class Calculator {
   }
 
 
-  checkPeriod = (string) => {
-    if(string === '.'){
-      this.remainProxy = '.';
-      if(this.addition.text){
-        this.screen.value = this.addition.text + " " + this.addition.operator + " " + this.currentView.text + this.remainProxy
-      }else{
-        this.screen.value = '' + this.currentView.text + this.remainProxy
-      }
-    }
-  }
+  // checkPeriod = (string) => {
+  //   if(string === '.'){
+  //     this.remainProxy = '.';
+  //     if(this.addition.text){
+  //       this.screen.value = this.addition.text + " " + this.addition.operator + " " + this.currentView.text + this.remainProxy
+  //     }else{
+  //       this.screen.value = '' + this.currentView.text + this.remainProxy
+  //     }
+  //   }
+  // }
   
-  deleteBackOne = (string) => {
-    if(!this.remainProxy.length && string === 'del') {
-      if(this.intProxy.length > 1){
-        this.intProxy = this.intProxy.slice(0,-1);
-      } else {
-        this.intProxy = '';
-      }
-    } 
-    if(this.remainProxy.length && string === 'del' ) {
+  // deleteBackOne = (string) => {
+  //   if(!this.remainProxy.length && string === 'del') {
+  //     if(this.intProxy.length > 1){
+  //       this.intProxy = this.intProxy.slice(0,-1);
+  //     } else {
+  //       this.intProxy = '';
+  //     }
+  //   } 
+  //   if(this.remainProxy.length && string === 'del' ) {
 
-    }
+  //   }
       
-  }
+  // }
 
-  set setCurrent(string) {
+  // set setCurrent(string) {
 
-    // console.log(string, !this.intProxy.length, typeof string );
+  //   // console.log(string, !this.intProxy.length, typeof string );
     
-    ///sets remainProxy
-    // this.checkPeriod();
+  //   ///sets remainProxy
+  //   // this.checkPeriod();
 
-    if(!this.remainProxy.length) {
-      // if(!this.intProxy.length && string == '0' || this.intProxy[0] == '0' && string == '0'){
-      //   console.log('zero');
-      //   this.intProxy = '0'; 
+  //   if(!this.remainProxy.length) {
+  //     // if(!this.intProxy.length && string == '0' || this.intProxy[0] == '0' && string == '0'){
+  //     //   console.log('zero');
+  //     //   this.intProxy = '0'; 
         
-      // } else {
-        if(string !== 'del'){
-          this.intProxy += string; 
-        } 
-        else {
-          if(this.intProxy.length > 1){
-            this.intProxy = this.intProxy.slice(0,-1);
-          } else {
-            this.intProxy = '';
-          }
-        }
-        // console.log(this.intProxy);
-      // }
-      // Add to intProxy
-      if(this.intProxy.length > 3) {
+  //     // } else {
+  //       if(string !== 'del'){
+  //         this.intProxy += string; 
+  //       } 
+  //       else {
+  //         if(this.intProxy.length > 1){
+  //           this.intProxy = this.intProxy.slice(0,-1);
+  //         } else {
+  //           this.intProxy = '';
+  //         }
+  //       }
+  //       // console.log(this.intProxy);
+  //     // }
+  //     // Add to intProxy
+  //     if(this.intProxy.length > 3) {
      
 
-          const newString = this.processToComma(this.intProxy)
+  //         const newString = this.processToComma(this.intProxy)
           
-          this.currentView.text = newString
+  //         this.currentView.text = newString
           
-        } else {
-          if(+this.intProxy){
-            if(this.currentView.text[0] == '0'){
-              const arrString = Array.from(this.currentView.text)
-              this.currentView.text = arrString.splice(1,1).join('');
-              // this.screen.value = this.currentView.text;
-              console.log(this.currentView.text);
-            }
-            console.log('int: ', this.intProxy);
-            console.log(this.intProxy[0] == '0', this.intProxy);
-            if(string !== 'del'){
+  //       } else {
+  //         if(+this.intProxy){
+  //           if(this.currentView.text[0] == '0'){
+  //             const arrString = Array.from(this.currentView.text)
+  //             this.currentView.text = arrString.splice(1,1).join('');
+  //             // this.screen.value = this.currentView.text;
+  //             console.log(this.currentView.text);
+  //           }
+  //           console.log('int: ', this.intProxy);
+  //           console.log(this.intProxy[0] == '0', this.intProxy);
+  //           if(string !== 'del'){
 
-              this.currentView.text += this.intProxy[this.intProxy.length - 1]
-            } else {
-              if (this.currentView.text.length === 5){
-                const arrCurrentView = Array.from(this.currentView.text)
-                const fixCurrentView = _.without(arrCurrentView, ',')
-                this.currentView.text = fixCurrentView.slice(0,-1).join('')
-              } else {
-                // console.log(this.currentView.text.slice(0,-1));
+  //             this.currentView.text += this.intProxy[this.intProxy.length - 1]
+  //           } else {
+  //             if (this.currentView.text.length === 5){
+  //               const arrCurrentView = Array.from(this.currentView.text)
+  //               const fixCurrentView = _.without(arrCurrentView, ',')
+  //               this.currentView.text = fixCurrentView.slice(0,-1).join('')
+  //             } else {
+  //               // console.log(this.currentView.text.slice(0,-1));
 
-                if(this.currentView.text.length >= 1){
+  //               if(this.currentView.text.length >= 1){
 
-                  this.currentView.text = this.currentView.text.slice(0,-1);
-                }
-              }
-            }
+  //                 this.currentView.text = this.currentView.text.slice(0,-1);
+  //               }
+  //             }
+  //           }
             
-          } else {
-            if(this.intProxy.length === 1 ){
+  //         } else {
+  //           if(this.intProxy.length === 1 ){
 
-              this.currentView.text = this.intProxy[0];
-            } else {
-              this.currentView.text = ''
-            }
-            this.intProxy = ''
-          }
-        }
+  //             this.currentView.text = this.intProxy[0];
+  //           } else {
+  //             this.currentView.text = ''
+  //           }
+  //           this.intProxy = ''
+  //         }
+  //       }
       
-      if(this.addition.text){
-          this.screen.value = this.addition.text + " " + this.addition.operator + " " + this.currentView.text;
-      }else{
-        this.screen.value = this.currentView.text;
-      }
-    }
+  //     if(this.addition.text){
+  //         this.screen.value = this.addition.text + " " + this.addition.operator + " " + this.currentView.text;
+  //     }else{
+  //       this.screen.value = this.currentView.text;
+  //     }
+  //   }
 
-    if(this.remainProxy.length && string !== '.' ) {
-      if(string !== 'del'){
-        //  add to remain proxy
-        this.remainProxy += string; 
+  //   if(this.remainProxy.length && string !== '.' ) {
+  //     if(string !== 'del'){
+  //       //  add to remain proxy
+  //       this.remainProxy += string; 
 
-        if(this.addition.text){
-          this.screen.value = this.addition.text + " " + this.addition.operator + " " + this.currentView.text + this.remainProxy
-        }else{
-          this.screen.value = '' + this.currentView.text + this.remainProxy
-        }
+  //       if(this.addition.text){
+  //         this.screen.value = this.addition.text + " " + this.addition.operator + " " + this.currentView.text + this.remainProxy
+  //       }else{
+  //         this.screen.value = '' + this.currentView.text + this.remainProxy
+  //       }
 
-      } else {
-        this.remainProxy = this.remainProxy.slice(0,-1);
+  //     } else {
+  //       this.remainProxy = this.remainProxy.slice(0,-1);
 
-        if(this.addition.text){
-          this.screen.value = this.addition.text + " " + this.addition.operator + " " + this.currentView.text + this.remainProxy
-        }else{
-          this.screen.value = '' + this.currentView.text + this.remainProxy
-        }
-      }
-    }
+  //       if(this.addition.text){
+  //         this.screen.value = this.addition.text + " " + this.addition.operator + " " + this.currentView.text + this.remainProxy
+  //       }else{
+  //         this.screen.value = '' + this.currentView.text + this.remainProxy
+  //       }
+  //     }
+  //   }
 
    
-  }
+  // }
 
 
 
@@ -189,60 +189,99 @@ export default class Calculator {
     console.log('sets string commas');
   }
 
-  enter = (string) => {
-    if(string === '.' && !this.intProxy.includes('.')){
-      this.setCurrent = string;
-    } else if (/[0-9]/.test(string)){
-      this.setCurrent = string;
-    }
-    // this.currentView.text += string; 
-    // this.screen.value = this.currentView.text;
-  }
-
-  operator = (string) => {
-    if(this.addition.length){
-      // const num1 = this.addition
-    }
-    if(!this.remainProxy.length){
-      // const total = +this.addition.text + +this.currentView.text;
-      // const totalWComma = this.processToComma(total)
-      this.addition.number = exactMath.add(+_.split(this.addition.text, ',').join(''), +_.split(this.currentView.text, ',').join(''));
-      this.addition.text = this.processToComma(this.addition.number)
-      // this.addition.text = +this.addition.text + +this.currentView.text;
-      this.addition.operator =  ` ${string} `;
-    } else {
-      this.addition.number = exactMath(+_.split(this.addition.number + +this.currentView.text, ',').join(''), +this.remainProxy) ;
-      this.addition.text = this.processToComma(this.addition.number) ;
-      this.addition.operator =  ` ${string} `;
-    }
-
-    
-
-    this.intProxy = "";
-    this.remainProxy = "";
-    this.currentView.text = "";
-    this.screen.value = this.addition.text + " " + this.addition.operator + " ";
-
-    console.log('addition: ', this.addition, typeof this.addition.number);
-    console.log('current: ', this.currentView);
-  }
  
+
   delete = (e) => {
       // console.log('event: ', e);
-      if(e){
-        this.setCurrent = 'del';
+    if(e){
+      //   this.setCurrent = 'del';
+      if(this.currentView.text.length > 1){
+        this.currentView.number = this.currentView.number.slice(0,-1)
+        this.currentView.text = this.processToComma(this.currentView.number)
+      } else {
+        this.currentView.text = this.addition.text;
+        this.currentView.number = this.addition.number;
+        this.addition = {
+          text: '',
+          number: '',
+          operator: ''
+        }
       }
+    }
+
+      this.print()
   }
 
   submit = () => {
-    console.log('=====');
+    if(this.currentView.text.length){
+      this.currentView.number = '' + exactMath.formula(this.addition.number + " " + this.addition.operator + " " + this.currentView.number);
+      this.currentView.text = this.processToComma(this.currentView.number)
+      this.addition.operator = '';
+      this.addition.number = ''
+      this.addition.text = ''
+    } else {
+      this.currentView.number = "" + this.addition.number
+      this.currentView.text = this.addition.text;
+      this.addition.operator = '';
+      this.addition.number = ''
+      this.addition.text = ''
+
+    }
+
+    console.log('submit');
+    this.print()
   }
 
+  print = () => {
+    this.addition.operator ? this.screen.value = this.addition.text + ' ' + this.addition.operator + ' ' + this.currentView.text : this.screen.value = this.currentView.text;
+    console.log(this.currentView);
+    console.log(this.addition);
+  }
+  
+  checkPeriod = () => {
+   if(this.currentView.number.includes('.')){
+     return false;
+   }
+   return true;
+  }
 
- handleInput = (string) => {
-    // console.log(string);
+  processNumber = (string) => {
+    if(string === '.'){
+
+      this.checkPeriod() ? this.currentView.number += string : this.currentView.number;
+    } else {
+
+      this.currentView.number += +string
+    }
+    this.currentView.text = this.processToComma(this.currentView.number);
+  }
+
+  operator = (string) => {
+    if(!this.addition.operator){
+      this.addition.number = this.currentView.number
+      this.addition.text = this.currentView.text
+      this.addition.operator = string;
+      this.currentView.number = ''
+      this.currentView.text = ''
+    } else if (this.currentView.number.length){
+      this.addition.number = "" + exactMath.formula(this.addition.number + " " + this.addition.operator + " " + this.currentView.number);
+      this.addition.text = this.processToComma(this.addition.number)
+      console.log(string);
+      this.addition.operator = string;
+      this.currentView.number = ''
+      this.currentView.text = ''
+    } else {
+      this.addition.operator = string;
+
+    }
+  }
+
+ handleRequest = (string) => {
+    console.log(string);
     if(/\/|\+|-|x/.test(string)){
         this.operator(string);
+        
+
     } 
     else if(/=/.test(string)){
         this.submit(string);
@@ -250,14 +289,15 @@ export default class Calculator {
     else if(/del/.test(string)){
         this.delete(string);
     } else if(/[0-9]|\./.test(string)){
-      this.enter(string);
+      this.processNumber(string)
     }
+    this.print(this.currentView.text)
     // this.screen.value = this.currentView.text
   }
 
   handleClick = (e) => {
     if(e.target.type === 'button'){
-     this.handleInput(e.target.value);
+     this.handleRequest(e.target.value);
       
     //  this.screen.value += e.target.value;
     }
@@ -274,14 +314,14 @@ export default class Calculator {
       if(/Backspace/.test(e.key)){
         this.delete(e)
       } else if(/[0-9]|\/|\+|-|x/.test(e.key)){
-        this.handleInput(e.key)
+        this.handleRequest(e.key)
       } 
     })
     window.addEventListener('keydown', e => {
       if(/Backspace/.test(e.key)){
         this.delete(e)
       } else if(/[0-9]|\/|\+|-|x|\./.test(e.key)){
-        this.handleInput(e.key)
+        this.handleRequest(e.key)
       } 
     })
   }
@@ -299,17 +339,15 @@ export default class Calculator {
     el.addEventListener('click', () => {
       this.currentView = {
         text: '',
-        number: null,
+        number: '',
       };
       this.addition = {
         text: '',
-        number: null,
+        number: '',
         operator: ''
       };
-      this.screen.value = '';
-      this.screen.innerHTML = '';
-      this.intProxy = "";
-      this.remainProxy = "";
+     this.print()
+    
       // console.log("reset");
       // console.log("screen: ", this.screen.value);
       // console.log("currentView.text: ", this.currentView.text);
@@ -318,7 +356,8 @@ export default class Calculator {
   configureSubmit = (el) => {
     el.parentElement.addEventListener('submit', (e) => {
       e.preventDefault();
-      console.log(this.addition, this.currentView.text);
+      this.submit()
+      // console.log(this.addition, this.currentView.text);
     })
   }
 
