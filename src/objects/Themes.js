7019deea149570d65a7e8273b93bea72769da4b9
William Owen <william.owen.bsc@gmail.com>
2021-10-23 18:31:37 -0700

    themes showing

--- src/objects/Themes.js
+++ src/objects/Themes.js
@@ -15,18 +15,62 @@ export default class Themes {
   setTheme = () => {
     switch (this.value) {
       case "1":
+        // main
         this.bodyEl.setAttribute('class','theme1')
+        window.localStorage.setItem('calc-theme', 'theme1')
         break;
       case "2":
+        // light
         this.bodyEl.setAttribute('class','theme2')
+        window.localStorage.setItem('calc-theme', 'theme2')
         break;
       case "3":
+        // dark
         this.bodyEl.setAttribute('class','theme3')
+        window.localStorage.setItem('calc-theme', 'theme3')
         break;
       default:
         break;
     }
     console.log(this.bodyEl, this.value);
+    console.log(window.localStorage);
+  }
+
+  setToggle = () => {
+    this.bodyEl.className
+    switch (this.bodyEl.className) {
+      case 'theme1':
+        this.input.value = 1
+        break;
+        case 'theme2':
+          this.input.value = 2
+          break;
+          case 'theme3':
+            this.input.value = 3     
+        break;
+    
+      default:
+        break;
+    }
+    // console.log(document.styleSheets);
+    // document.styleSheets[0].insertRule('input[type=range]::-webkit-slider-thumb {visibility: visible}', 0);
+  }
+
+  checkForPrefersColor = () => {
+    
+    if(window.matchMedia('(prefers-color-scheme: light)').matches){
+      this.bodyEl.setAttribute('class','theme2')
+    }
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+      this.bodyEl.setAttribute('class','theme3')
+    }
+    if(window.localStorage.getItem('calc-theme')){
+      const theme = window.localStorage.getItem('calc-theme')
+      console.log(theme);
+      this.bodyEl.setAttribute('class',`${theme}`)
+      // console.log('found a theme');
+    }
+    this.setToggle()
   }
 
   configure = () => {
@@ -34,6 +78,7 @@ export default class Themes {
       this.value = e.target.value;
       this.setTheme();
     })
+    this.checkForPrefersColor()
   }
 
 }
